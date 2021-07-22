ab0d9544caa3126d8e74260034aff0e53840a77a
TEGDV <apjair97@gmail.com>
2021-07-22 16:54:12 -0500

    :recycle: 4.1 Refactor Raindrops using iterators

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,17 +1,18 @@
-pub fn raindrops(n: u32) -> String {
-    let mut result = String::new();
+const DROPS: [(u32, &str); 3] = [(3, "Pling"), (5, "Plang"), (7, "Plong")];
 
-    if n % 3 == 0 {
-        result.push_str("Pling")
+fn is_factor(n: u32, factor: u32) -> bool {
+    n % factor == 0
+}
+
+pub fn raindrops(n: u32) -> String {
+    let sounds = DROPS
+        .iter()
+        .filter_map(|(drop, sound)| is_factor(n, *drop).then(|| *sound))
+        .collect::<String>();
+
+    if !sounds.is_empty() {
+        return sounds;
     }
-    if n % 5 == 0 {
-        result.push_str("Plang")
-    }
-    if n % 7 == 0 {
-        result.push_str("Plong")
-    }
-    if result == "" {
-        result = n.to_string()
-    }
-    result
+
+    n.to_string()
 }
